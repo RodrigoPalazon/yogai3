66d5250668bb59695570ce83f5903ba41461f759
chris t <ctselepidas@gmail.com>
2017-05-27 22:47:59 +0200

    cleanup and reset br function

--- bin/br-reset.py
+++ bin/br-reset.py
@@ -9,11 +9,8 @@ def main():
 	#get all workspaces and find the focused monitor
     workspaces = i3.get_workspaces()
     workspace = [w['output'] for w in workspaces if w['focused'] is True][0]
-    # get current brightness of focused monitor
-    output = subprocess.check_output("xrandr --verbose | awk '/%s/{f=1}/Brightness:/ && f{print $2; exit}'" %workspace, shell=True)
     #issue command to change brightness
-    newBrightnessValue = float(output)-0.2
-    setBrightness = 'xrandr --output %s --brightness %s' %(workspace, newBrightnessValue)
+    setBrightness = 'xrandr --output %s --brightness %s' %(workspace, 1)
     process = subprocess.Popen(setBrightness, shell=True, stdout=subprocess.PIPE)
 
 if __name__ == '__main__':
--- bin/br-up.py
+++ bin/br-up.py
@@ -3,12 +3,18 @@
 # Script to swap which workspace is on which monitor (output)
 # Might not work as expected if at least one workspace is empty
 
-import i3, os, subprocess
-from pprint import pprint
+import i3, subprocess
 
 def main():
+	#get all workspaces and find the focused monitor
     workspaces = i3.get_workspaces()
     workspace = [w['output'] for w in workspaces if w['focused'] is True][0]
-    os.system( 'xrandr --output %s --brightness 1' %workspace)
+    # get current brightness of focused monitor
+    output = subprocess.check_output("xrandr --verbose | awk '/%s/{f=1}/Brightness:/ && f{print $2; exit}'" %workspace, shell=True)
+    #issue command to change brightness
+    newBrightnessValue = float(output)+0.2
+    setBrightness = 'xrandr --output %s --brightness %s' %(workspace, newBrightnessValue)
+    process = subprocess.Popen(setBrightness, shell=True, stdout=subprocess.PIPE)
+
 if __name__ == '__main__':
     main()
